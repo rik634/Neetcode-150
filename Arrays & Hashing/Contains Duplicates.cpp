f3cf4ec9c3a9562638b676a2d0b7593b765045ce
Riya Kotalwar <83593359+rik634@users.noreply.github.com>
2025-12-15 13:24:20 +0530

    Create  Contains Duplicates.cpp

---
+++ Arrays & Hashing/Contains Duplicates.cpp
@@ -0,0 +1,18 @@
+// Level: easy
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(nums[i]==nums[i+1])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+// Time complexity: O(nlogn)
+// space complexity: O(1)

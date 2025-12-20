a4c051707a3370624353fb7791c1a6e659929489
Riya Kotalwar <83593359+rik634@users.noreply.github.com>
2025-12-20 23:42:44 +0530

    Create Two sum II - Input array is sorted.cpp

---
+++ Two Pointers/Two sum II - Input array is sorted.cpp
@@ -0,0 +1,34 @@
+// leveL: medium
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        
+        int n = numbers.size();
+        
+        for(int i=0;i<n;i++)
+        {
+            int t = target-numbers[i];
+            int l=i+1;
+            int r = n-1;
+            while(l<=r)
+            {
+                int mid = l + (r-l)/2;
+                if(numbers[mid]==t)
+                {
+                    return {i+1,mid+1};
+                }
+                else if(numbers[mid]<t)
+                {
+                    l=mid+1;
+                }
+                else
+                {
+                    r=mid-1;
+                }
+            }
+        }
+        return {};
+    }
+};
+// time complexity: O(nlogn)
+// space complexity: O(1)

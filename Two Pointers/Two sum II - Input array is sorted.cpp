d64c1ccce1dfd68103ffd25f9d33b5f85d62a611
Riya Kotalwar <83593359+rik634@users.noreply.github.com>
2025-12-20 23:49:06 +0530

    Update Two sum II - Input array is sorted.cpp

--- Two Pointers/Two sum II - Input array is sorted.cpp
+++ Two Pointers/Two sum II - Input array is sorted.cpp
@@ -1,4 +1,5 @@
 // leveL: medium
+// binary search
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
@@ -31,4 +32,63 @@ public:
     }
 };
 // time complexity: O(nlogn)
+// space complexity: O(1)
+
+// hashmap
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        
+        int n = numbers.size();
+        unordered_map<int,int> mp;
+        for(int i=0;i<n;i++)
+        {
+            int t = target-numbers[i];
+            if(mp.find(t)==mp.end())
+            {
+                mp[numbers[i]]=i;
+            }
+            else{
+                if((i+1)<(mp[t]+1))
+                {
+                    return {i+1,mp[t]+1};
+                }
+                return {mp[t]+1,i+1};
+                
+            }
+        }
+        return {};
+    }
+};
+// time complexity: O(n)
+// space complexity: O(n)
+
+// two pointers
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        
+        int n = numbers.size();
+        int l=0;
+        int r=n-1;
+        while(l<=r)
+        {
+            int sum = numbers[l]+numbers[r];
+            if(sum==target)
+            {
+                return {l+1,r+1};
+            }
+            else if(sum<target)
+            {
+                l++;
+            }
+            else
+            {
+                r--;
+            }
+        }
+        return {};
+    }
+};
+// time complexity: O(n)
 // space complexity: O(1)

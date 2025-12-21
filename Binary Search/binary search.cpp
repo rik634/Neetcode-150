b08edbb45ed572893f02f2ae458a1a5bb089a8ea
Riya Kotalwar <83593359+rik634@users.noreply.github.com>
2025-12-21 16:41:43 +0530

    Create binary search.cpp

---
+++ Binary Search/binary search.cpp
@@ -0,0 +1,29 @@
+// level: easy
+// binary search algorithm
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int n = nums.size();
+        int l=0;
+        int r=n-1;
+        while(l<=r)
+        {
+            int mid = l + (r-l)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(nums[mid]<target)
+            {
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return -1;
+    }
+};
+// time complexity: O(logn)
+// space complexity: O(1)
